b9762ee6392c876df4d98c5130cb0bf11b84bd11
ewangarn83@gmail.com <ewangarn83@gmail.com>
2025-11-27 19:30:54 +0100

    Quelques changement sur MP2

--- MP2/MP2_EWAN_GARNAUD.js
+++ MP2/MP2_EWAN_GARNAUD.js
@@ -1,10 +1,26 @@
 var saisit = prompt("Saisissez votre texte :");
+
 if(saisit.startsWith("http://") || saisit.startsWith("https://")){
+
+    var domaine = saisit.substring(11,saisit.length)
+    var domainehtt = saisit.substring(12,saisit.length)
+    var html = document.getElementById("texte");
+
+    if(saisit.startsWith("http://")){
+        html.innerText = domaine.toLocaleLowerCase();
+        html.setAttribute("href",saisit)
+    }
+    else{
+        html.innerText = domainehtt.toLocaleLowerCase();
+        html.setAttribute("href",saisit);
+    }
 }
 else{
+
     var phrase = "http://" + saisit;
     var html = document.getElementById("texte");
-    html.innerText = phrase.toLocaleLowerCase();
-    html.setAttribute("href",phrase);
-    console.log(phrase);
-}
+    var domaine = phrase.substring(11,phrase.length)
+
+    html.innerText = domaine.toLocaleLowerCase();
+    html.setAttribute("href",phrase)
+    }
