b80a2faa4637d5af20cd710c0c54eb0c3c686d73
ewangarn83@gmail.com <ewangarn83@gmail.com>
2025-11-27 21:03:44 +0100

    Quelques modif du MP3

--- MP3/MP3_EWAN_GARNAUD.js
+++ MP3/MP3_EWAN_GARNAUD.js
@@ -1,15 +1,26 @@
 var pourcentage_pourboire = 10
-var console = false
-function calculatrice_pb(montant_commande,pourcentage_pourboire,console){
+var affichage = false
+
+function calculatrice_pb(montant_commande,pourcentage_pourboire,affichage){
     pourcentage_pourboire = pourcentage_pourboire / 100;
-    var pourboire = montant_commande / pourcentage_pourboire;
+    var pourboire = montant_commande * pourcentage_pourboire;
     var avis = prompt("Tu veux tout voir dans la console o/n");
+
     if(avis == 'o'){
-        console = true;
+        affichage = true;
     }
-    if(console){
+    if(affichage){
         console.log(montant_commande,pourboire);
     } 
     return pourboire ;
 }
-console.log(calculatrice_pb(100));
+
+var montant = Number(prompt("Montant de la commande ?"));
+var pourcentage = prompt("Pourcentage de pourboire ?");
+if (pourcentage === "" || pourcentage === null) {
+    pourcentage = 10;
+} else {
+    pourcentage = Number(pourcentage);
+}
+var resultat = calculatrice_pb(montant, pourcentage, false);
+alert("Le pourboire est : " + resultat + "€");
